889157323cf6d2b38befb616ea7ab3261a5efaee
AyushRaj1329 <ayushraj790673@gmail.com>
2025-08-20 14:26:43 +0530

    added sample.cpd

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from logic.parser import parse_cpd_cards
 def test_parser():
     file_path = "data/sample.CPD"   # relative path from project root
-    paired_cards = parse_cpd_cards("C:/Users/khushi yadav/OneDrive/Documents/Pyhton projects/card_sequence_validator/Card-Sequence-Validator/data/sample.CPD")
+    paired_cards = parse_cpd_cards("C:/Users/DELL/Downloads/sample.CPD")
 
     for current, next_card in paired_cards:
         print(f"Current: {current}")
